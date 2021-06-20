769470559a58746915d548d007d7a2d8204c26cb
Colin Siles <colin.siles16@gmail.com>
2021-06-20 12:53:25 -0600

    Implement functional active section detection

--- components/header.jsx
+++ components/header.jsx
@@ -1,8 +1,35 @@
 import Link from 'next/link'
 import { Twirl as Hamburger } from 'hamburger-react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import classNames from 'classnames'
 import styles from './styles/header.module.css'
+
+// Section ids and the corresponding header text for each section of the home page
+const SECTIONS = {'about-section': 'About Me',
+                  'portfolio-section': 'Portfolio', 
+                  'post-section': 'Posts'}
+
+/**
+ * Obtains the scroll position of the given element 
+ * @param {string} sectionId the id of the section of which to get the scroll position for
+ * @returns {Number} an integer specifying the scroll position of the top of the element
+ */
+function getScrollY(sectionId) {
+    const element = document.getElementById(sectionId);
+
+    return element.getBoundingClientRect().top
+}
+
+/**
+ * Builds the initial state to track the section scroll positions
+ * @param {Array} sections array of section ids
+ * @returns {Object} of (sectionId, DOM element) pairs
+ */
+function getScrollPositions(sections) {
+    return Object.fromEntries(
+        sections.map(sectionId => [sectionId, getScrollY(sectionId)])
+    )
+}
 
 /**
  * The header displayed on all parts of the site 
@@ -11,7 +38,34 @@ import styles from './styles/header.module.css'
  * @returns {ReactElement}
  */
 export default function Header({ home }) {
-    const [navOpen, setNavOpen] = useState(false);
+    const [navOpen, setNavOpen] = useState(false); // Track if the navbar menu is open on small devices
+    const [activeSectionId, setActiveSectionId] = useState(null); // Track which section id is active
+
+    // Mount an event listener to track and update the scroll positions 
+    // Note: this isn't a very React way to do this. I think we should be using refs
+    useEffect(() => {
+        const updateActive = () => {
+            const scrollPositions = getScrollPositions(Object.keys(SECTIONS));
+
+            let newActiveSection = activeSectionId;
+
+            for (const [sectionId, position] of Object.entries(scrollPositions)) {
+                if (position < 100) {
+                    newActiveSection = sectionId
+                }
+            }
+    
+            setActiveSectionId(newActiveSection); 
+        }
+
+        updateActive()
+        window.addEventListener('scroll', updateActive);
+
+        // Remove the event listener on unmount
+        return () => {
+            window.removeEventListener('scroll', updateActive);
+        }
+    }, [])
 
     return (
         <header className={styles.header}>
@@ -27,9 +81,11 @@ export default function Header({ home }) {
 
             { home &&
                 <nav className={classNames(styles.nav, {[styles.open]: navOpen })}>
-                    <a href='/#about-section'><h2>About Me</h2></a>
-                    <a href='/#portfolio-section'><h2>Portfolio</h2></a>
-                    <a href='/#post-section'><h2>Posts</h2></a>
+                    {Object.entries(SECTIONS).map(([sectionId, text]) => (
+                        <a href={`/#${sectionId}`}  key={sectionId}>
+                            <h2 className={classNames({[styles.active]: activeSectionId === sectionId})}>{text}</h2>
+                        </a>
+                    ))}
                 </nav>
             }
         </header>
