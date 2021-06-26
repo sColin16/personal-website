e946aec0e8f61dee36716d469283ab51b6c42cf3
Colin Siles <colin.siles16@gmail.com>
2021-06-25 20:39:32 -0600

    Transfer project card styles and structure

--- components/portfolioSection.jsx
+++ components/portfolioSection.jsx
@@ -1,5 +1,49 @@
 import HomeSectionLayout from "./homeSectionLayout"
 import CardCollection from "./cardCollection"
+import ProjectCard from "./projectCard"
+
+const projects = [
+    {
+        id: 'visplay',
+        name: 'Visplay',
+        thumbnail: '/visplay-icon.svg',
+        description: 'A digital signage solution that makes improvement on the existing software controlling digital displays around Mine\'s campus',
+        links: [ {'href': 'https://gitlab.com/ColoradoSchoolOfMines/visplay', 'icon': 'fab fa-gitlab'}, {'href': 'https://coloradoschoolofmines.gitlab.io/visplay/', 'icon': 'fas fa-external-link-alt'} ],
+        tags: ['Distributed Systems', 'Backend']
+    },
+    {
+        id: 'visplay',
+        name: 'Visplay',
+        thumbnail: '/visplay-icon.svg',
+        description: 'A digital signage solution that makes improvement on the existing software controlling digital displays around Mine\'s campus',
+        links: [ {'href': 'https://gitlab.com/ColoradoSchoolOfMines/visplay', 'icon': 'fab fa-gitlab'}, {'href': 'https://coloradoschoolofmines.gitlab.io/visplay/', 'icon': 'fas fa-external-link-alt'} ],
+        tags: ['Distributed Systems', 'Backend']
+    },
+    {
+        id: 'visplay',
+        name: 'Visplay',
+        thumbnail: '/visplay-icon.svg',
+        description: 'A digital signage solution that makes improvement on the existing software controlling digital displays around Mine\'s campus',
+        links: [ {'href': 'https://gitlab.com/ColoradoSchoolOfMines/visplay', 'icon': 'fab fa-gitlab'}, {'href': 'https://coloradoschoolofmines.gitlab.io/visplay/', 'icon': 'fas fa-external-link-alt'} ],
+        tags: ['Distributed Systems', 'Backend']
+    },
+    {
+        id: 'visplay',
+        name: 'Visplay',
+        thumbnail: '/visplay-icon.svg',
+        description: 'A digital signage solution that makes improvement on the existing software controlling digital displays around Mine\'s campus',
+        links: [ {'href': 'https://gitlab.com/ColoradoSchoolOfMines/visplay', 'icon': 'fab fa-gitlab'}, {'href': 'https://coloradoschoolofmines.gitlab.io/visplay/', 'icon': 'fas fa-external-link-alt'} ],
+        tags: ['Distributed Systems', 'Backend']
+    },
+    {
+        id: 'visplay',
+        name: 'Visplay',
+        thumbnail: '/visplay-icon.svg',
+        description: 'A digital signage solution that makes improvement on the existing software controlling digital displays around Mine\'s campus',
+        links: [ {'href': 'https://gitlab.com/ColoradoSchoolOfMines/visplay', 'icon': 'fab fa-gitlab'}, {'href': 'https://coloradoschoolofmines.gitlab.io/visplay/', 'icon': 'fas fa-external-link-alt'} ],
+        tags: ['Distributed Systems', 'Backend']
+    },
+]
 
 export default function PortfolioSection() {
     return (
@@ -7,11 +51,9 @@ export default function PortfolioSection() {
             <h1>Portfolio</h1>
 
             <CardCollection initialCards={3} increment={2} displayDelay={100}>
-                <p>Project 1</p>
-                <p>Project 2</p>
-                <p>Project 3</p>
-                <p>Project 4</p>
-                <p>Project 5</p>
+                {projects.map(project => (
+                    <ProjectCard projectInfo={project} key={project.id}/>
+                ))}
             </CardCollection>
         </HomeSectionLayout>
     )
---
+++ components/projectCard.jsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import styles from './styles/projectcard.module.css'
+
+export default function ProjectCard({ projectInfo }) {
+    return (
+        <div className={styles.outer_container}>
+            <Link href={`/project/${projectInfo.id}`}>
+                <div className={styles.inner_container}>
+                    <div className={styles.thumbnail_container}>
+                        <img src={projectInfo.thumbnail} />
+                    </div>
+                    <div className={styles.info_container}>
+                        <h2>{projectInfo.name}</h2>
+                        <p>{projectInfo.description}</p>
+                    </div>
+                    <div className={styles.tag_container}>
+                        {projectInfo.tags.map(tag => (
+                            <span key={tag}>{tag}</span>
+                        ))}
+                    </div>
+                </div>
+            </Link>
+
+            <div className={styles.links_container}>
+                {projectInfo.links.map(link => (
+                    <a href={link.href} key={link.href}>
+                        <i className={link.icon}></i>
+                    </a>
+                ))}
+            </div>
+        </div>
+    )
+}
