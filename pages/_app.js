1e7e331f1071fc55772fbc4d2cb778df10a28459
Colin Siles <colin.siles16@gmail.com>
2021-06-25 22:26:26 -0600

    Clean up template and old code

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles_next/globals.css'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles_next/Home.module.css'
 import Layout from '../components/layout'
 import AboutSection from '../components/aboutSection'
 import PortfolioSection from '../components/portfolioSection'
