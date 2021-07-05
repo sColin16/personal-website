7af0c3eb7ee01594259e3420721995d414432b25
Colin Siles <colin.siles16@gmail.com>
2021-07-05 11:31:06 -0600

    All card collections same the same stylings

--- components/postsSection.jsx
+++ components/postsSection.jsx
@@ -1,13 +1,12 @@
 import HomeSectionLayout from "./homeSectionLayout"
 import CardCollection from "./cardCollection"
 import PostCard from "./postCard"
-import styles from "./styles/postsection.module.css"
 
 export default function PostsSection({ posts }) {
     return (
         <HomeSectionLayout id="post-section">
             <h1>Posts</h1>
-            <CardCollection initialCards={3} increment={2} displayDelay={100} wrapperClass={styles.card_constraints} containerClass={styles.container_constraints}>
+            <CardCollection initialCards={3} increment={2} displayDelay={100}>
                 {posts.map(post => (
                     <PostCard postInfo={post} key={post.id}/>
                 ))}
